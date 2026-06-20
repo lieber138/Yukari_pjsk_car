4467b4b07fcb6fdfd08e8ef74e659b1d3eecb79f
lieber138 <3625408198@qq.com>
2026-06-20 10:51:04 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,9 +5,9 @@ from twikit import Client
 async def main():
     client = Client(language='ja-JP')
     await client.login(
-        auth_info_1='Yukari_3281',
-        auth_info_2='3907203281@qq.com',  # 如果上面填的是用户名，这里填邮箱
-        password='z3625408198'
+        auth_info_1='',
+        auth_info_2='',  # 如果上面填的是用户名，这里填邮箱
+        password=''
     )
     client.save_cookies('cookies.json')
     print('登录成功，cookie 已保存')
